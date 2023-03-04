ae4a7e80ff59fd588276475952a8f1c8a01a1765
Maycon Jardim <mayconsmill@gmail.com>
2023-03-03 21:35:24 -0300

    Projeto criado

---
+++ mmaliga-backend/src/main/java/com/mmaliga/MmaligaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mmaliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaligaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaligaBackendApplication.class, args);
+	}
+
+}
---
+++ mmaliga-backend/src/test/java/com/mmaliga/MmaligaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmaliga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaligaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
