84e170f360c9c8dfe7007727bce74013db171097
Maycon Jardim <mayconsmill@gmail.com>
2023-03-04 23:50:15 -0300

    FighterRepository

---
+++ mmaliga-backend/src/main/java/com/mmaliga/repositories/FighterRepository.java
@@ -0,0 +1,9 @@
+package com.mmaliga.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mmaliga.entities.Fighter;
+
+public interface FighterRepository extends JpaRepository<Fighter, Long> {
+
+}
