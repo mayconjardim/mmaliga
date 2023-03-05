696468c43edea39ef5e0dd551815c607b8268375
Maycon Jardim <mayconsmill@gmail.com>
2023-03-04 23:59:53 -0300

    Fighter Resource, findbyId ok

--- mmaliga-backend/src/main/java/com/mmaliga/resources/FighterResource.java
+++ mmaliga-backend/src/main/java/com/mmaliga/resources/FighterResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class FighterResource {
     public ResponseEntity<List<FighterDTO>> findAll() {
     	List<FighterDTO> page = fighterService.findAll();
         return ResponseEntity.ok().body(page);
+    }
+    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<FighterDTO> findById(@PathVariable long id) throws Exception {
+    	FighterDTO dto = fighterService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 	
 }
