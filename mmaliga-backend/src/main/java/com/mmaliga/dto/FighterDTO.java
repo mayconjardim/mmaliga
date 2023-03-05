5e313d4ee14b4be1b3652b8a9fa2ed609afb577b
Maycon Jardim <mayconsmill@gmail.com>
2023-03-04 23:48:57 -0300

    FighterDTO

---
+++ mmaliga-backend/src/main/java/com/mmaliga/dto/FighterDTO.java
@@ -0,0 +1,83 @@
+package com.mmaliga.dto;
+
+
+
+import java.io.Serializable;
+
+import com.mmaliga.entities.Fighter;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter
+@Getter
+@NoArgsConstructor
+public class FighterDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EqualsAndHashCode.Include
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String nickname;
+	private Integer age;
+	// private WeightClass weightClass;
+	// private fightingStyle fightingStyle;
+
+	// Habilidaddes
+	private Integer boxing;
+	private Integer clinch;
+	private Integer bjj;
+	private Integer kicking;
+	private Integer wrestling;
+
+	// Fisico
+	private Integer strength;
+	private Integer agility;
+	private Integer conditioning;
+	private Integer toughness;
+	private Integer koResistance;
+	
+	public FighterDTO(Long id, String firstName, String lastName, String nickname, Integer age, Integer boxing,
+			Integer clinch, Integer bjj, Integer kicking, Integer wrestling, Integer strength, Integer agility,
+			Integer conditioning, Integer toughness, Integer koResistance) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.nickname = nickname;
+		this.age = age;
+		this.boxing = boxing;
+		this.clinch = clinch;
+		this.bjj = bjj;
+		this.kicking = kicking;
+		this.wrestling = wrestling;
+		this.strength = strength;
+		this.agility = agility;
+		this.conditioning = conditioning;
+		this.toughness = toughness;
+		this.koResistance = koResistance;
+	}
+	
+	public FighterDTO(Fighter entity) {
+		id = entity.getId();
+		firstName = entity.getFirstName();
+		lastName = entity.getLastName();
+		nickname = entity.getNickname();
+		age = entity.getAge();
+		boxing = entity.getBoxing();
+		clinch = entity.getClinch();
+		bjj = entity.getBjj();
+		kicking = entity.getKicking();
+		wrestling = entity.getWrestling();
+		strength = entity.getStrength();
+		agility = entity.getAgility();
+		conditioning = entity.getConditioning();
+		toughness = entity.getToughness();
+		koResistance = entity.getKoResistance();
+	}
+	
+}
